22680925c834a31c372accfe14d7622fb98fe11e
kentok1234 <aditia.putra100@gmail.com>
2020-04-30 16:05:13 +0700

    menambah inisialisasi ke main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import pygame
 from pygame.locals import *
 
+class spaceship():
+    def __init__(self):
+        pass
+    def player(self):
+        pass
+    def bot(self):
+        pass
+    def movement(self):
+        pass
+    def screenWindow(self):
+        pass
+
+if __name__ == "__main__":
+    pass
