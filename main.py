56011a08e3cc1b76e3a643d7e0d6eff8a2b2665b
kentok1234 <aditia.putra100@gmail.com>
2020-05-02 17:20:49 +0700

    set ship can fire and print fps at top corner

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ from pygame.locals import *
 import random
 
 # Inisialisasi game
-pygame.init ()
+pygame.init()
 
+clock = pygame.time.Clock()
+frame_per_second = 60
 width = 500
 height = 500
 screen = pygame.display.set_mode((width, height))
+
+# set default font
+font_default = pygame.font.get_default_font()
+font = pygame.font.Font(font_default, 12)
+
+#print fps
+print_fps = font.render('fps : {}'.format(int(clock.get_fps())), True, (255,255,255))
 
 # Set icon, title and screen
 icon = pygame.image.load('icon.png')
@@ -37,22 +46,38 @@ alien_moveX = []
 alien_moveY = []
 e_respawnX = random.randint(0,500)
 e_respawnY = -(alien.get_width()*alien_num)
-distance = 0
+distanceX = 0
+distanceY = 0
 
 for i in range(alien_num):
     alien_group.append(alien)
-    alien_X.append(e_respawnX+distance)
-    alien_Y.append(e_respawnY+distance) 
+    alien_X.append(e_respawnX+distanceX)
+    alien_Y.append(e_respawnY+distanceY) 
     alien_moveX.append(0.5)
     alien_moveY.append(0.1) 
-    distance += 32
+    distanceX += 64
+    distanceY += 32
 def draw_alien(img, x, y, i):
     screen.blit(img[i], (x[i], y[i]))
-            
+
+# bullet
+bullet_width = 5
+bullet_height = 5
+bullet_X = 0
+bullet_Y = 0
+speed_bullet = 1
+bullet_state = 'ready'
+def draw_bullet(x, y):
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, bullet_width, bullet_height))
+def ship_fire(x, y):
+    bullet_state = 'fire'
+    draw_bullet(x, y)
 
 # Game running
 running = True
 while running:
+
+    
 
     screen.blit(background, (0, 0))
 
@@ -68,6 +93,11 @@ while running:
                 s_move_Y = -0.5
             if event.key == pygame.K_DOWN:
                 s_move_Y = 0.5
+            if event.key == pygame.K_SPACE:
+                bullet_X = shipX + (spaceship.get_width() /2) - 2
+                bullet_Y = shipY
+                bullet_state = 'fire'
+                ship_fire(bullet_X, bullet_Y)
         #Key up
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
@@ -106,5 +136,11 @@ while running:
             alien_X[i] = 0
             alien_moveX[i] = 0.5  
         draw_alien(alien_group, alien_X, alien_Y, i)
+    
+    # bullet move
+    if bullet_state == 'fire':
+        bullet_Y -= speed_bullet
+        ship_fire(bullet_X, bullet_Y)
 
+    screen.blit(print_fps, (0, 0)) 
     pygame.display.update()
